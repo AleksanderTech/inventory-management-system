f8bc2f2ebf4215b7ae76f1c8c9d9f7a89e779ffe
alexecute <aleksanderroztrop@gmail.com>
2026-01-06 11:40:50 +0100

    feat: add product creation command

--- src/app/dependencies.ts
+++ src/app/dependencies.ts
@@ -1,11 +1,14 @@
 import type { Knex } from "knex";
 import { GetProductsQuery } from "../modules/product/queries/get-products-query.ts";
+import { CreateProductCommand } from "../modules/product/commands/create-product-command.ts";
 
 export const createDependencies = async ({ db }: { db: Knex }) => {
   const getProductsQuery = new GetProductsQuery(db);
+  const createProductCommand = new CreateProductCommand(db);
 
   return {
     getProductsQuery,
+    createProductCommand,
   };
 };
 
--- src/modules/product/model/types.ts
+++ src/modules/product/model/types.ts
@@ -1,4 +1,4 @@
-import { ProductCategory } from "../../shared/contracts/product/model/constants";
+import { ProductCategory } from "../../shared/contracts/product/model/constants.ts";
 
 // app
 export type ProductView = {
@@ -10,5 +10,24 @@ export type ProductView = {
   stock: number;
 };
 
+export type CreateProductInput = {
+  name: string;
+  description: string;
+  category: ProductCategory;
+  priceMinor: number;
+  stock: number;
+};
+
+export type CreateProductOutput = {
+  id: string;
+  name: string;
+  description: string;
+  category: ProductCategory;
+  priceMinor: number;
+  stock: number;
+};
+
 // api
 export type GetProductsResponse = ProductView[];
+export type CreateProductRequest = CreateProductInput;
+export type CreateProductResponse = CreateProductOutput;
